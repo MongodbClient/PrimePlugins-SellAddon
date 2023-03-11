2c1a56a3df473d62ca861a624eca1aa2a631c2ab
MongodbClient <drach.jonas@web.deX>
2023-03-11 11:17:00 +0100

    Initial commit

---
+++ src/main/java/de/mongodbclient/primepluginsaddon/selladdon/listener/InventoryClickEventListener.java
@@ -0,0 +1,10 @@
+package de.mongodbclient.primepluginsaddon.selladdon.listener;
+
+/**
+ * @author Jonas D. | MongodbClient 
+ * Created on 11.03.2023
+ * Created for PrimePlugins-SellAddon
+ **/
+ 
+public class InventoryClickEventListener {
+}
